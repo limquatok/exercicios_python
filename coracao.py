a046cfb1bbf20f320595de4528953b78eb605d40
limquatok <isaquemaropo@gmail.com>
2023-09-07 17:05:14 -0300

    loop Corações

---
+++ coracao.py
@@ -0,0 +1,27 @@
+# loop de Corações <3
+
+
+import time
+
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+
+print(len(grid))
+
+while True:
+
+    for y in range(6):
+        print()
+        for x in range(len(grid)): 
+            print(grid[x][y] , end=' ')
+
+    time.sleep(1.0)
